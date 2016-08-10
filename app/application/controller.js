8cad20ea2c0f8ece87518b6d0007bb6a75fc8cb8
Jonathan Fuqua <jfuqua390@gmail.com>
2016-08-10 16:05:31 -0500

    Initial Markup and function start

--- app/application/controller.js
+++ app/application/controller.js
@@ -3,6 +3,7 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
 
   parse() {
-    console.log("yo")
+    const preamble = `We the People of the United States, in Order to form a more perfect Union, establish Justice, insure domestic Tranquility, provide for the common defence, promote the general Welfare, and secure the Blessings of Liberty to ourselves and our Posterity, do ordain and establish this Constitution for the United States of America.`;
+    console.log(preamble);
   },
 });
--- app/router.js
+++ app/router.js
@@ -7,6 +7,7 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('logout');
+  this.route('parse');
 });
 
 export default Router;
