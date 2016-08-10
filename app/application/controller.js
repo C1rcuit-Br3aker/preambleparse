f09e9040503e2c26e8d50e478d5ab01f5d4c8f5b
Jonathan Fuqua <jfuqua390@gmail.com>
2016-08-10 17:25:34 -0500

    User can now input any string he/she would like

--- app/application/controller.js
+++ app/application/controller.js
@@ -3,12 +3,18 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   numTs: 0,
   numEs: 0,
+  numTEs: 0,
 
-  parse() {
-    const preamble = `We the People of the United States, in Order to form a more perfect Union, establish Justice, insure domestic Tranquility, provide for the common defence, promote the general Welfare, and secure the Blessings of Liberty to ourselves and our Posterity, do ordain and establish this Constitution for the United States of America.`;
-    const nopunc = this.removePunc(preamble);
+  usePreamble() {
+    const preamble = "We the People of the United States, in Order to form a more perfect Union, establish Justice, insure domestic Tranquility, provide for the common defence, promote the general Welfare, and secure the Blessings of Liberty to ourselves and our Posterity, do ordain and establish this Constitution for the United States of America.";
+    this.parse(preamble);
+  },
+
+  parse(str) {
+    const nopunc = this.removePunc(str);
     this.numberOfT(nopunc);
     this.numberOfE(nopunc);
+    this.both(nopunc);
   },
 
   removePunc(str) {
@@ -43,5 +49,19 @@ export default Ember.Controller.extend({
       } else return 0;
     });
     this.set(`numEs`, numE);
+  },
+
+  both(str) {
+    const arr = str.split(" ");
+    var count = 0;
+
+    arr.forEach((word) => {
+      if(word.startsWith("t") && word.endsWith("e")) {
+        return count++;
+      }else if(word.startsWith("T") && word.endsWith("e")) {
+        return count++;
+      } else return 0;
+    });
+    this.set(`numTEs`, count);
   },
 });
